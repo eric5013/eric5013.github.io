54632f899ddf663880892d11b0dccb6f2ba1d9a5
Eric Zeng <admin@zengjianpeng.com>
2019-02-02 17:27:24 +0800

    Update jump.js

--- jump.js
+++ jump.js
@@ -1,1 +1,15 @@
-alert("jump success!");
+console.log('预加载成功');
+//定义跳转页面
+var pageNum = 0;
+
+//若为1，则跳转主页面
+//若为0，则跳转维护页面
+
+
+if (pageNum == 1) {
+	window.location.href="404/index.html";
+	console.log('加载维护页面');
+} else {
+	window.location.href="main.html"
+	console.log('加载主页面');
+}
